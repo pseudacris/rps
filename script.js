5f45e63fd7aaa1d5e6d92f0e0627e0982fcda658
pseudacris <hyliola@pm.me>
2024-05-27 14:49:03 -0700

    Start score tracking function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function getComputerChoice() {
-     let computerChoice = Math.random();
-        if (computerChoice < 1 / 3) {
+function getComputerChoice(choice) {
+        choice = Math.random();
+        if (choice < 1 / 3) {
             return 'rock';
-        } else if (computerChoice < 2 / 3) {
+        } else if (choice < 2 / 3) {
             return 'paper';
         } else {
             return 'scissors';
@@ -26,3 +26,20 @@ function getHumanChoice(choice) {
 }
 
 console.log(getHumanChoice())
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    const noCase = humanChoice.toLowerCase();
+    const rock = 'rock';
+    const paper = 'paper';
+    const scissors = 'scissors';
+    
+    if (noCase === 'rock' && computerChoice === )
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
