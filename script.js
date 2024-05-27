d0a5ff228c4c71cbe864d8095297fa9a1c5760e9
pseudacris <hyliola@pm.me>
2024-05-27 13:38:29 -0700

    Create functions for the computer choices and human choices

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function getComputerChoice() {
-    return 
+     let computerChoice = Math.random();
+        if (computerChoice < 1 / 3) {
+            return 'rock';
+        } else if (computerChoice < 2 / 3) {
+            return 'paper';
+        } else {
+            return 'scissors';
+        }
 }
+
+console.log(getComputerChoice());
+
+function getHumanChoice(choice) {
+    choice = prompt('Please enter rock, paper, or scissors.');
+
+    if (choice === 'rock') {
+        return 'You chose rock.';
+    } else if (choice === 'paper') {
+        return 'You chose paper.';
+    } else if (choice === 'scissors') {
+        return 'You chose scissors.';
+    } else {
+        alert('Invalid selection! Try again.');
+    }
+}
+
+console.log(getHumanChoice())
