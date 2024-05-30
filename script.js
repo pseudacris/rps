7cb16e91b70aa7d079bd89bc26cb681cb2097da2
pseudacris <hyliola@pm.me>
2024-05-29 21:38:23 -0700

    Stop prompt reappearing and game results from being wrong

--- script.js
+++ script.js
@@ -1,58 +1,65 @@
 function getComputerChoice(choice) {
-        choice = Math.random();
-        if (choice < 1 / 3) {
-            return 'rock';
-        } else if (choice < 2 / 3) {
-            return 'paper';
-        } else {
-            return 'scissors';
-        }
+    choice = Math.random();
+    if (choice < 1 / 3) {
+        return 'rock';
+    } else if (choice < 2 / 3) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
 }
 
-console.log(getComputerChoice());
+let compChoice = getComputerChoice();
+console.log(compChoice);
 
 function getHumanChoice(choice) {
     choice = prompt('Please enter rock, paper, or scissors.');
-    // choice = choice.toLowerCase();
-    if (choice === 'rock') {
-        return 'You chose rock.';
-    } else if (choice === 'paper') {
-        return 'You chose paper.';
-    } else if (choice === 'scissors') {
-        return 'You chose scissors.';
+    choice = choice.toLowerCase();
+    if (choice === 'rock' || 
+        choice === 'paper' || 
+        choice === 'scissors') {
+            return choice;
+
     } else {
         alert('Invalid selection! Try again.');
-    }
-
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = choice.toLowerCase();
-        computerChoice = getComputerChoice();
-        if (humanChoice === computerChoice) {
-            return 'It\'s a tie!';
-        } else if ( 
-            (humanChoice === 'rock' &&
-        computerChoice === 'scissors') ||
-            (humanChoice === 'paper' && 
-        computerChoice === 'rock') ||
-            (humanChoice === 'scissors' && 
-        computerChoice === 'paper')
-        ) {
-            return 'The human wins!';
-        } else {
-            return 'The computer wins!';
-        }
-
+        
     }
 }
 
-// console.log(getHumanChoice())
+let choice = getHumanChoice();
+console.log(`You chose ${choice}.`);
 
-// let humanScore = 0;
-// let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = choice;
+const computerSelection = compChoice;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return 'It\'s a tie!';
+    } else if ( 
+        (humanChoice === 'rock' &&
+    computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && 
+    computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && 
+    computerChoice === 'paper')
+    ) {
+        return 'The human wins!';
+    } else {
+        return 'The computer wins!';
+    }
+}
+
+
+console.log(playRound(humanSelection, computerSelection));
 
 
 
 
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
+
+
+
+
 
