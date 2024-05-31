ba9f3662c4d7ce50faf3ba4360df6ec0d8668653
pseudacris <hyliola@pm.me>
2024-05-30 20:39:29 -0700

    Nest playRound in playGame

--- script.js
+++ script.js
@@ -27,50 +27,38 @@ function getHumanChoice(choice) {
 let choice = getHumanChoice();
 console.log(`You chose ${choice}.`);
 
-let humanScore = 0;
-let computerScore = 0;
-
 const humanSelection = choice;
 const computerSelection = compChoice;
 
-function playRound(humanChoice, computerChoice) {
-    let result;
-    if (humanChoice === computerChoice) {
-        return 'It\'s a tie!';
-    } else if ( 
-        (humanChoice === 'rock' &&
-    computerChoice === 'scissors') ||
-        (humanChoice === 'paper' && 
-    computerChoice === 'rock') ||
-        (humanChoice === 'scissors' && 
-    computerChoice === 'paper')
-    ) {
-        result = 'Player wins!';        
-        console.log(result);
-        humanScore++;
-        console.log(`Player score: ${humanScore}`);
-    } else {
-        result = 'You lose!';
-        console.log(result);
-        computerScore++;
-        console.log(`Computer score: ${computerScore}`);
-    }
-    // if (humanChoice === 'The human wins!') {
-    //     humanScore++;
-    //     console.log(`Human score is: ${humanScore}`);
-    // } else if (computerChoice === 'The computer wins!') {
-    //     computerScore ++;
-    //     console.log(`Computer score is: ${computerScore}`);
-    // } else {
-        
-    //     console.log(`Human score is: ${humanScore}`);
-    //     console.log(`Computer score is: ${computerScore}`);
-    //     }
-    
-    
-}
+function playGame() {
 
-console.log(playRound(humanSelection, computerSelection));
+    function playRound(humanChoice, computerChoice) {
+        let result;
+        if (humanChoice === computerChoice) {
+            return 'It\'s a tie!';
+        } else if ( 
+            (humanChoice === 'rock' &&
+        computerChoice === 'scissors') ||
+            (humanChoice === 'paper' && 
+        computerChoice === 'rock') ||
+            (humanChoice === 'scissors' && 
+        computerChoice === 'paper')
+        ) {
+            result = 'Player wins!';        
+            console.log(result);
+            humanScore++;
+            console.log(`Player score: ${humanScore}`);
+        } else {
+            result = 'You lose!';
+            console.log(result);
+            computerScore++;
+            console.log(`Computer score: ${computerScore}`);
+        }
+        
+    }
+    console.log(playRound(humanSelection, computerSelection));   
+}   
+
 
 
 
